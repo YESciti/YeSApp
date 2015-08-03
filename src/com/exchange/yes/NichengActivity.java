43b1df594cfc53292d0772ee76d1ac6761d04404
lyk001 <649287196@qq.com>
2015-08-03 15:24:17 +0800

    test
    check

---
+++ src/com/exchange/yes/NichengActivity.java
@@ -0,0 +1,22 @@
+package com.exchange.yes;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class NichengActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_nicheng);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.nicheng, menu);
+		return true;
+	}
+
+}
