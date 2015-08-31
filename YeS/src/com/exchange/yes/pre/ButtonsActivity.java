0d81327ffdcc47d7366ece05407ef6f0642db068
lyk001 <649287196@qq.com>
2015-08-31 11:39:06 +0800

    831更新

---
+++ YeS/src/com/exchange/yes/pre/ButtonsActivity.java
@@ -0,0 +1,18 @@
+package com.exchange.yes.pre;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.DialogInterface.OnClickListener;
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Toast;
+
+
+
+public class ButtonsActivity extends Activity{
+	
+}
