791fee83d4de6dff643c022f9fcaccaa50b4485a
lyk001 <649287196@qq.com>
2015-09-09 20:37:58 +0800

    无能为力

---
+++ YeS/src/com/exchange/yes/util/IsNetWorkAvailable.java
@@ -0,0 +1,69 @@
+package com.exchange.yes.util;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+
+
+public class IsNetWorkAvailable 
+{
+	
+	public static void setNetWork(Context context,final Activity activity)
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		builder.setIcon(android.R.drawable.ic_dialog_alert);
+		builder.setTitle("网络状�??");
+		builder.setMessage("当前无可用网络，请设�?");
+		builder.setPositiveButton("设置", new OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				activity.startActivity(new Intent(android.provider.Settings.ACTION_SETTINGS));				
+			}
+		});
+		builder.setNegativeButton("取消", new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+		
+		builder.create();
+		builder.show();
+	}
+	
+	//判断是否连接
+	public static boolean isAvailable(Context context)
+	{
+		//备注
+		ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		if(connectivityManager == null)
+		{
+			return false;
+		}
+		else {
+			//获得NetworkInfo对象    获取�?有网络连�?
+			NetworkInfo[] infos = connectivityManager.getAllNetworkInfo();
+			if(infos!=null)
+			{
+				for(int i=0;i<infos.length;i++)
+				{
+					//判断是否连接网络
+					if(infos[i].getState()==NetworkInfo.State.CONNECTED)
+                    {
+                        return true;
+                    }
+				}
+			}
+		}
+		return false;
+		
+	}
+}
